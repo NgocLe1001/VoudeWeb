19a79b4bf246d5c933986ff40ad2ff9a3479f8a4
NgocLe1001 <ngocthile1001@gmail.com>
2020-05-05 16:50:58 +0700

    layout

---
+++ Voude/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Voude.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+          
+        }
+        public ActionResult CouponsPage()
+        {
+            return View();
+
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult DetailPromotion()
+        {
+            return View();
+        }
+        public ActionResult CategoryPromotion()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+      
+    }
+}
